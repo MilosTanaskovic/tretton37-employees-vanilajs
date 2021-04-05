ca6cdf897848c473ae1fc73b4d05d0d67b69e4fb
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2021-04-05 23:14:13 +0200

    Implement Scroll to top func and styling it...

--- asset/js/toggle.js
+++ asset/js/toggle.js
@@ -1,5 +1,8 @@
 const wrapper = document.querySelector('.wrapper');
 const chk = document.getElementById('chk');
+const target = document.querySelector('footer');
+const scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+const rootElement = document.documentElement
 
 // Grid-List view toggle
 
@@ -25,3 +28,32 @@ chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
   window.scrollTo(0,0);
 });
+
+// Scroll to top
+
+const callback = (entries, observer) => {
+  // The callback will return an array of entries, even if you are only observing a single item
+  entries.map(entry => {
+    if (entry.isIntersecting) {
+      // Show button
+      scrollToTopBtn.classList.add("showBtn")
+    } else {
+      // Hide button
+      scrollToTopBtn.classList.remove("showBtn")
+    }
+  });
+}
+
+const scrollToTop = () => {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+    
+// Next I instantiate the observer with the function I created above. This takes an optional configuration
+// object that I will use in the other examples.
+let observer = new IntersectionObserver(callback);
+// Finally start observing the target element
+observer.observe(target);
