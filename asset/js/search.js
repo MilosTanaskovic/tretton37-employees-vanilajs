a14e46a496312c6c7ac7b2db489cdcedf15433ff
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2021-04-05 22:02:19 +0200

    implement func Sort by name and office...

--- asset/js/search.js
+++ asset/js/search.js
@@ -1,5 +1,23 @@
-const main = () => {
+const search = document.getElementById("search");
 
+// onSearch function
+const onSearch = e => {
+ console.log(e.target.value);
+ const value = e.target.value.trim();
+ cache.currentEmployees = cache.employees.filter( employee => {
+  console.log(employee);
+  return (
+   (employee.office && employee.office.toLowerCase().indexOf(value.toLowerCase()) === 0) || 
+   (employee.email && employee.email.toLowerCase().indexOf(value.toLowerCase()) === 0) ||
+   (employee.name && employee.name.toLowerCase().indexOf(value.toLowerCase()) === 0)
+ )
+ })
+ showEmployees();
+}
+
+const main = () => {
+ search.addEventListener("input", onSearch)
+ console.log(search);
  getEmployees();
 }
 window.onload = () => {
